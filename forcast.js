fd87608c626dc63b2cb565c0ce0dab08996adfd1
fvillegas341 <100173407+fvillegas341@users.noreply.github.com>
2022-07-14 11:39:35 -0600

    add files via upload

---
+++ forcast.js
@@ -0,0 +1,32 @@
+const apiKey = '11y6adimU2t8G37UGbsG0Y6r0MibXi5W'; //create a variable for the API Key
+
+
+//get weather information
+const getWeather = async (id) => {
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/"; 
+    const query = `${id}?apikey=${apiKey}`;
+
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+
+
+
+//get city information
+const getLocation = async (city) => {
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${apiKey}&q=${city}`;
+
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
